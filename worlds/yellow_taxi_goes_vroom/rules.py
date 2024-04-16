eafbbf5c4fae5fa9fa84aff6fa5c45ed57694d91
panicbit <panicbit.dev@gmail.com>
2024-04-17 00:43:51 +0200

    add access rules for all areas

--- worlds/yellow_taxi_goes_vroom/rules.py
+++ worlds/yellow_taxi_goes_vroom/rules.py
@@ -4,25 +4,46 @@ from typing import TYPE_CHECKING, Dict
 
 from worlds.generic.Rules import CollectionRule
 
+from .subclasses import YTGVItem
+
 if TYPE_CHECKING:
     from . import YTGVWorld
 
 class YTGVRules:
     world: YTGVWorld
     connection_rules: Dict[str, CollectionRule]
+    event_bombcar: YTGVItem
+    event_noble_knight: YTGVItem
+    event_orange_button: YTGVItem
+    event_now_i_remember: YTGVItem
+    event_she_is_fine_now: YTGVItem
 
     def __init__(self, world: YTGVWorld):
         self.world = world
+        self.event_doggo = self.world.create_item("Doggo")
+        self.event_bombcar = self.world.create_item("Bomboss defeated")
+        self.event_noble_knight = self.world.create_item("Noble Knight!")
+        self.event_orange_button = self.world.create_item("Orange Button")
+        self.event_now_i_remember = self.world.create_item("Now I remember!")
+        self.event_she_is_fine_now = self.world.create_item("She is fine now!")
         player = world.player
         options = world.options
 
         self.connection_rules = {
-            # Mosk's rocket is only available after beating Granny
+            "Granny's Island -> Ice-Cream truck":
+                lambda state: state.has(self.event_bombcar.name, player),
+            "Granny's Island -> Pizza Oven":
+                lambda state: state.has(self.event_noble_knight.name, player),
+            "Granny's Island -> Fecal Matters":
+                lambda state: state.has(self.event_doggo.name, player),
+            "Granny's Island -> Crash Again":
+                lambda state: state.has(self.event_orange_button.name, player),
+            "Granny's Island -> Flushed Away":
+                lambda state: state.has(self.event_orange_button.name, player),
             "Granny's Island -> Moon":
-                lambda state: state.has("She is fine now!", player),
+                lambda state: state.has(self.event_she_is_fine_now.name, player),
             "Granny's Island -> Mosk's Rocket":
-                lambda state: state.has("She is fine now!", player),
-            # Portal gear requirements
+                lambda state: state.has(self.event_she_is_fine_now.name, player),
             "Morio's Lab -> Morio's Island":
                 lambda state: state.has("Gear", player, options.morios_island_required_gears),
             "Morio's Lab -> Bombeach":
@@ -37,8 +58,19 @@ class YTGVRules:
                 lambda state: state.has("Gear", player, options.maurizios_city_required_gears),
             "Morio's Lab -> Crash Test Industries":
                 lambda state: state.has("Gear", player, options.crash_test_industries_required_gears),
+            "Morio's Lab -> Morio's Mind":
+                lambda state: state.has(self.event_orange_button.name, player),
+            "Morio's Lab -> Observing":
+                lambda state: (
+                    state.has(self.event_orange_button.name, player) and
+                    state.has(self.event_now_i_remember.name, player)
+                ),
             "Morio's Lab -> Anticipation":
-                lambda state: state.has("Gear", player, options.anticipation_required_gears),
+                lambda state: (
+                    state.has(self.event_orange_button.name, player) and
+                    state.has(self.event_now_i_remember.name, player) and
+                    state.has("Gear", player, options.anticipation_required_gears)
+                ),
         }
 
     def set(self):
@@ -49,7 +81,13 @@ class YTGVRules:
             entrance = multiworld.get_entrance(entrance_name, player)
             entrance.access_rule = rule
 
+        # Place events
+        multiworld.get_location("Bombcar", player).place_locked_item(self.event_bombcar)
+        multiworld.get_location("Doggo", player).place_locked_item(self.event_doggo)
+        multiworld.get_location("Pizza King", player).place_locked_item(self.event_noble_knight)
+        multiworld.get_location("Orange Button", player).place_locked_item(self.event_orange_button)
+        multiworld.get_location("Passkey", player).place_locked_item(self.event_now_i_remember)
+        multiworld.get_location("Granny", player).place_locked_item(self.event_she_is_fine_now)
+
         # Set up completion
-        event_she_is_fine_now = self.world.create_item("She is fine now!");
-        multiworld.get_location("Granny", player).place_locked_item(event_she_is_fine_now)
-        multiworld.completion_condition[player] = lambda state: state.has(event_she_is_fine_now.name, player)
+        multiworld.completion_condition[player] = lambda state: state.has(self.event_she_is_fine_now.name, player)
--- worlds/yellow_taxi_goes_vroom/subclasses.py
+++ worlds/yellow_taxi_goes_vroom/subclasses.py
@@ -25,9 +25,29 @@ class YTGVRegion(Region):
         self.add_locations(id_by_location, YTGVLocation)
 
         # Event locations
+        if self.name == "Morio's Lab":
+            doggo = YTGVLocation(world.player, "Doggo", None, self)
+            self.locations.append(doggo)
+
+        if self.name == "Bombeach":
+            bombcar = YTGVLocation(world.player, "Bombcar", None, self)
+            self.locations.append(bombcar)
+
+        if self.name == "Pizza Time":
+            pizza_king = YTGVLocation(world.player, "Pizza King", None, self)
+            self.locations.append(pizza_king)
+
+        if self.name == "Crash Test Industries":
+            orange_button = YTGVLocation(world.player, "Orange Button", None, self)
+            self.locations.append(orange_button)
+
+        if self.name == "Morio's Mind":
+            pass_key = YTGVLocation(world.player, "Passkey", None, self)
+            self.locations.append(pass_key)
+
         if self.name == "Spaceship":
             granny = YTGVLocation(world.player, "Granny", None, self)
-            self.locations.append(granny);
+            self.locations.append(granny)
 
         self.multiworld.regions.append(self)
 
