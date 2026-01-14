449cbd67aa048000b44e4f080c26a0c3ddba5f08
Bastien Orivel <eijebong@bananium.fr>
2026-01-14 18:14:00 +0100

    Don't batch releases twice

--- MultiServer.py
+++ MultiServer.py
@@ -1283,18 +1283,11 @@ def register_location_checks(ctx: Context, team: int, slot: int, locations: typi
             log_lines.append('(Team #%d) %s sent %s to %s (%s)' % (
                 team + 1, ctx.player_names[(team, slot)], ctx.item_names[ctx.slot_info[target_player].game][item_id],
                 ctx.player_names[(team, target_player)], ctx.location_names[ctx.slot_info[slot].game][location]))
-            if len(info_texts) >= 140:
-                # split into chunks that are close to compression window of 64K but not too big on the wire
-                # (roughly 1300-2600 bytes after compression depending on repetitiveness)
-                ctx.broadcast_team(team, info_texts)
-                info_texts.clear()
             info_texts.append(json_format_send_event(new_item, target_player))
+
         ctx.broadcast_team(team, info_texts)
         if log_lines:
             ctx.logger.info('\n'.join(log_lines))
-        del info_texts
-        del log_lines
-        del sortable
 
         ctx.location_checks[team, slot] |= new_locations
         send_new_items(ctx, affected_slots)
